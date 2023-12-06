4f142ca9b9a1e37e275aef1fa0de13c3b75e6907
astickystick <153083055+astickystick@users.noreply.github.com>
2023-12-06 18:21:05 +0300

    Update Task_1.py

--- Task_1.py
+++ Task_1.py
@@ -50,3 +50,116 @@ board = [[1, 2, 3], [4, 4, 5], [5, 3, 4]]
 # Example: moving the gem at position (1, 1) to the right
 result = is_legal_move(board, 1, 1, 'right')
 result
+
+
+
+#Task 2 
+# Checking if a move is legal on a 4x4 board
+def is_legal_move(board, row, col, direction):
+    # Check boundaries and move gem
+    if direction == 'up' and row > 0:
+        board[row][col], board[row-1][col] = board[row-1][col], board[row][col]
+    elif direction == 'down' and row < 3:
+        board[row][col], board[row+1][col] = board[row+1][col], board[row][col]
+    elif direction == 'left' and col > 0:
+        board[row][col], board[row][col-1] = board[row][col-1], board[row][col]
+    elif direction == 'right' and col < 3:
+        board[row][col], board[row][col+1] = board[row][col+1], board[row][col]
+    else:
+        return False  # Invalid move
+
+    # Check for a line of 4 same gems horizontally or vertically
+    if check_line(board, row, col) or check_line(board, row-1, col) or check_line(board, row+1, col) or check_line(board, row, col-1) or check_line(board, row, col+1):
+        # Move is legal, revert the swap and return True
+        revert_swap(board, row, col, direction)
+        return True
+    else:
+        # Move is illegal, revert the swap and return False
+        revert_swap(board, row, col, direction)
+        return False
+
+# Adjusting the check_line function for a 4x4 grid
+def check_line(board, row, col):
+    # Check if the position is within the board
+    if 0 <= row < 4 and 0 <= col < 4:
+        gem = board[row][col]
+        # Check horizontally and vertically
+        return (col > 0 and col < 3 and gem == board[row][col-1] == board[row][col+1]) or \
+               (row > 0 and row < 3 and gem == board[row-1][col] == board[row+1][col])
+    return False
+
+# Example 4x4 game board
+board = [[1, 2, 3, 4], [4, 4, 5, 6], [5, 3, 4, 7], [8, 7, 6, 5]]
+
+# Test on 4x4 board
+# Example: moving the gem at position (1, 1) to the right
+result = is_legal_move(board, 1, 1, 'right')
+
+result, board
+
+
+# Task 3 
+
+# Function to check for 'L' shape
+def check_l_shape(board, row, col, gem):
+    # 'L' shape can be in four orientations
+    # Each orientation has 3 gems in a line and 2 additional gems at a right angle at one end
+    # Checking all orientations
+    if row < len(board) - 2 and col < len(board[0]) - 1:
+        if all(board[row][c] == gem for c in range(col, col + 2)) and \
+           all(board[r][col] == gem for r in range(row + 1, row + 3)):
+            return True
+    if row < len(board) - 2 and col > 0:
+        if all(board[row][c] == gem for c in range(col - 1, col + 1)) and \
+           all(board[r][col] == gem for r in range(row + 1, row + 3)):
+            return True
+    # Vertical with horizontal tail
+    if row < len(board) - 1 and col < len(board[0]) - 2:
+        if all(board[row][c] == gem for c in range(col, col + 3)) and \
+           board[row + 1][col] == gem:
+            return True
+    if row > 0 and col < len(board[0]) - 2:
+        if all(board[row][c] == gem for c in range(col, col + 3)) and \
+           board[row - 1][col] == gem:
+            return True
+    return False
+
+# Function to check for 'T' shape
+def check_t_shape(board, row, col, gem):
+    # 'T' shape can be in four orientations
+    # Each orientation has 3 gems in a line and 1 additional gem at the center extending perpendicular
+    # Checking all orientations
+    if row < len(board) - 1 and col < len(board[0]) - 2 and col > 0:
+        if all(board[row][c] == gem for c in range(col - 1, col + 2)) and \
+           board[row + 1][col] == gem:
+            return True
+    if row > 0 and col < len(board[0]) - 2 and col > 0:
+        if all(board[row][c] == gem for c in range(col - 1, col + 2)) and \
+           board[row - 1][col] == gem:
+            return True
+    # Vertical with horizontal tail
+    if row < len(board) - 2 and col < len(board[0]) - 1:
+        if all(board[r][col] == gem for r in range(row, row + 3)) and \
+           board[row + 1][col + 1] == gem:
+            return True
+    if row < len(board) - 2 and col > 0:
+        if all(board[r][col] == gem for r in range(row, row + 3)) and \
+           board[row + 1][col - 1] == gem:
+            return True
+    return False
+
+# Function to check if a move results in an 'L' or 'T' shape
+def is_special_shape(board, row, col, gem):
+    if check_l_shape(board, row, col, gem):
+        return "L"
+    if check_t_shape(board, row, col, gem):
+        return "T"
+    return None
+
+# Example 4x4 game board with a potential 'T' shape
+board = [[1, 2, 3, 4], [4, 4, 5, 6], [5, 3, 4, 7], [8, 7, 6, 5]]
+
+# Test a move on the 4x4 board that should form a 'T' shape
+result, shape = is_legal_move_extended(board, 2, 2, 'left')
+
+result, shape, board
